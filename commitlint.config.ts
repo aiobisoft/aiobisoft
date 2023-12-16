de880107c1df8eb908ef0167c45d9a0fcf76fcd0
abubakar2000 <abubakar.mughal2ooo@icloud.com>
2023-12-17 02:22:42 +0500

    chore: adding commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,39 @@
+import { UserConfig } from "@commitlint/types";
+
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "ci",
+        "build",
+        "perf",
+        "revert",
+      ],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "example",
+        "config",
+        // Add your project-specific scopes here
+      ],
+    ],
+    "subject-case": [2, "always", "lowerCase"], // Enforce sentence case for the subject
+    // "subject-full-stop": [0, "never"], // Do not enforce a period at the end of the subject
+    "body-max-line-length": [2, "always", 72], // Enforce a maximum line length for the body
+    "footer-max-line-length": [2, "always", 72], //
+  },
+};
+
+export default config;
